06f5c7c5978006bbc738d9d6bfbe3d41f7619a0d
sidewinder2020 <sdwinder1@gmail.com>
2017-08-28 00:13:42 -0600

    create update functionality, partial, flash notice, root to index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
 
   def index
     @articles = Article.all
@@ -15,22 +16,33 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' has been Created!"
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path
   end
 
   def edit
   end
 
+  def update
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
